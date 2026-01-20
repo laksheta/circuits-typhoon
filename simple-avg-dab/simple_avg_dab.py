1b3feadee761aa99e52a232333818d3f3bcf9bc7
Laksheta <krishnalaksheta@gmail.com>
2026-01-20 16:49:25 +0700

    simple_avg_dab - setup

---
+++ simple-avg-dab/simple_avg_dab.py
@@ -0,0 +1,11 @@
+# Numpy module is imported as 'np'
+# Scipy module is imported as 'sp'
+# The Schematic API is imported as 'mdl'
+# To get the model file path, use 'mdl.get_model_file_path()'
+# To print information to the console, use info()
+import numpy as np
+
+fsw = 10e3
+
+V1  = 780
+V2  = 780
