d0b1ad0e0cca0ef6715404a903e3e30228df1e9b
Laksheta <krishnalaksheta@gmail.com>
2026-02-12 12:13:15 +0700

    code(simple-rect): replace ^ with **

---
+++ simple-avg-rectifier/simple_avg_rectifier.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+V_grid_peak = 220*np.sqrt(2)
+F           = 50
+omega       = 2*np.pi*F
+
+R           = 1e-1
+L           = 1e-3
+
+R_load      = 100
+C_out       = 10e-6
+
+V_dc_target = 800
+
+P_load = (V_dc_target**2) / R_load
+
+i_d_req = -(2 * P_load) / (3 * V_grid_peak)
+i_q_req = 0
+
+u_d = V_grid_peak - (R * i_d_req - omega * L * i_q_req)
+u_q = 0 - (R * i_q_req + omega * L * i_d_req)
+
+U_rect_amp = np.sqrt(u_d**2 + u_q**2)
+delta_rad  = np.atan2(u_q, u_d)
+delta      = delta_rad * (180/np.pi)
+
+m = U_rect_amp / V_dc_target
+
+# phase = [0+delta -120+delta 120+delta]
+
+phase = np.array([0+delta, -120+delta, 120+delta])
+
+V_rect = m*V_dc_target
+
