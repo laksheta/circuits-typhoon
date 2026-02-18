676d3b94faeb6171e021c37cf9fde9ec3ea18d5d
Laksheta <krishnalaksheta@gmail.com>
2026-02-18 10:24:28 +0700

    code(rect-dab): add add rectifier code

--- avg-rect-dab/rect_dab.py
+++ avg-rect-dab/rect_dab.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+#################
+#################
+#######DAB#######
+#################
+#################
+
 fsw_dab = 10e3
 T_dab = 1/fsw_dab
 
@@ -44,3 +50,62 @@ omega_c_dab = 2*np.pi*f_bw_dab
 
 K_p_dab      = (tau_dab*omega_c_dab)/Gdc_dab
 K_i_dab      = K_p_dab/tau_dab
+
+
+
+#################
+#################
+####Rectifier####
+#################
+#################
+
+exec_meas_rect   = 1e-6
+exec_sine_rect   = 1e-6
+exec_controller_rect = 1e-6
+
+V_grid_peak_rect = 220*np.sqrt(2)
+V_grid_rms_rect  = 220
+F_rect           = 50
+omega_rect       = 2*np.pi*F_rect
+
+f_sw_rect        = 50e3
+
+R_rect           = 1e-1
+L_rect           = 1e-3
+
+R_load_rect      = 100
+C_out_rect       = 10e-6
+
+V_dc_target_rect = 780
+
+P_load_rect = (V_dc_target_rect**2) / R_load_rect
+
+i_d_req_rect = -(2 * P_load_rect) / (3 * V_grid_peak_rect)
+i_q_req_rect = 0
+
+u_d_rect = V_grid_peak_rect - (R_rect * i_d_req_rect - omega_rect * L_rect * i_q_req_rect)
+u_q_rect = 0 - (R_rect * i_q_req_rect + omega_rect * L_rect * i_d_req_rect)
+
+U_rect_amp_rect = np.sqrt(u_d_rect**2 + u_q_rect**2)
+U_rect_rms_rect = U_rect_amp_rect/np.sqrt(2)
+delta_rad_rect  = np.arctan2(u_q_rect, u_d_rect)
+delta_rect      = delta_rad_rect * (180/np.pi)
+
+m_rect = U_rect_amp_rect / V_dc_target_rect
+
+phase_rect = [0+delta_rect, -120+delta_rect, 120+delta_rect]
+phase_3ph_rect    = delta_rect
+
+V_rect_rect = m_rect*V_dc_target_rect
+
+# Controller
+f_bw_i_rect      = f_sw_rect*0.1
+omega_ci_rect    = 2*np.pi*f_bw_i_rect
+Kp_i_rect        = omega_ci_rect * L_rect
+Ki_i_rect        = omega_ci_rect * R_rect
+
+f_bw_v_rect      = 20
+omega_cv_rect    = 2*np.pi*f_bw_v_rect
+zeta_rect        = 0.707
+Kp_v_rect        = 2*zeta_rect*omega_cv_rect*C_out_rect
+Ki_v_rect        = (omega_cv_rect**2)*C_out_rect
