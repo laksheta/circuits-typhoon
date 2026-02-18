55edaf63a536181691ebd871ad2723eb6ecb87ce
Laksheta <krishnalaksheta@gmail.com>
2026-02-18 07:55:33 +0700

    code(closed-dab): test equal execution time

--- closed-loop-avg-dab/closed_loop_avg_dab.py
+++ closed-loop-avg-dab/closed_loop_avg_dab.py
@@ -9,10 +9,10 @@ fsw = 10e3
 T = 1/fsw
 
 T_meas = 0.1e-6
-T_wave  = 1e-6  
+T_wave  = 0.1e-6  
 T_math  = 0.1e-6
-T_delay = 1e-6
-T_plant = 1e-6
+T_delay = 0.1e-6
+T_plant = 0.1e-6
 
 delay_deg   = 45
 delay_N = ((delay_deg/360)*T)/T_delay
