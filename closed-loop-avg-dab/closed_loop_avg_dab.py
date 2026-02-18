7f96bdc6b62f7a8191aeac1c0af30a6051db0c6e
Laksheta <krishnalaksheta@gmail.com>
2026-02-18 08:20:55 +0700

    code(closed-dab): improve clarity

--- closed-loop-avg-dab/closed_loop_avg_dab.py
+++ closed-loop-avg-dab/closed_loop_avg_dab.py
@@ -1,22 +1,16 @@
-# Numpy module is imported as 'np'
-# Scipy module is imported as 'sp'
-# The Schematic API is imported as 'mdl'
-# To get the model file path, use 'mdl.get_model_file_path()'
-# To print information to the console, use info()
 import numpy as np
 
 fsw = 10e3
 T = 1/fsw
 
 T_meas = 0.1e-6
-T_wave  = 0.1e-6  
+T_wave  = 1e-6  
 T_math  = 0.1e-6
 T_delay = 1e-6
 T_plant = 1e-6
 
 delay_deg   = 45
 delay_N = ((delay_deg/360)*T)/T_delay
-# print(delay_N)
 
 V_in  = 780
 V_out = 780
@@ -33,14 +27,10 @@ deg = d*(90/0.5)
 phi = (deg/360)*(1/fsw)
 
 phi_pu = 1-phi*fsw
-# print(phi_pu)
 
 I = V1*D*(1-D)/(2*fsw*L)
 R_load = V1/I
 
-# print(I)
-
-# PI Controller
 P_c = 5e3
 R_load_c  = (V_out**2) / P_c 
 
@@ -54,5 +44,3 @@ omega_c = 2*np.pi*f_bw
 
 K_p      = (tau*omega_c)/Gdc
 K_i      = K_p/tau
-
-print(R_load_c)
