e509af4346dbb5f3faecdadd0686842f4367d045
Laksheta <krishnalaksheta@gmail.com>
2026-02-22 20:21:29 +0700

    typhoon(dab-inv): rename variable (1)

--- avg-dab-inv/dab_inv.py
+++ avg-dab-inv/dab_inv.py
@@ -119,7 +119,7 @@ if 'info' not in globals():
 # sig_select = [1]
 
 # T_meas = 1e-6
-tau = 1e-3
+# tau = 1e-3
 
 V_dc = 780
 upper_lim = +V_dc/2
@@ -137,23 +137,23 @@ F = 50
 phase = np.array([0,-120,120])
 
 L = 1e-3
-R = L / tau
+# R = L / tau
 
 i_d = 10
 i_q = 0
 omega   = 2*np.pi*F
 
-u_d = u_grid_d + R*i_d - omega*L*i_q
-u_q = u_grid_q + R*i_q + omega*L*i_d
+# u_d = u_grid_d + R*i_d - omega*L*i_q
+# u_q = u_grid_q + R*i_q + omega*L*i_d
 
-U_inv_amp = np.sqrt(u_d**2+u_q**2)
-U_inv_ph  = np.arctan2(u_q, u_d)
+# U_inv_amp = np.sqrt(u_d**2+u_q**2)
+# U_inv_ph  = np.arctan2(u_q, u_d)
 
 f_wb_i = 1e3
 omega_c = 2*np.pi*f_wb_i
 
 Kp_i    = omega_c*L
-Ki_i    = omega_c*R
+# Ki_i    = omega_c*R
 
-info(f"u_d: {u_d:.2f}")
-info(f"u_q: {u_q:.2f}")
+# info(f"u_d: {u_d:.2f}")
+# info(f"u_q: {u_q:.2f}")
