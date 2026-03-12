391b3504f3ede04c248ba23628288df3c7cdbdfb
Laksheta <krishnalaksheta@gmail.com>
2026-03-12 10:23:24 +0700

    code(cl-dab): add PI scripts

--- new-closed-loop-dab/new_closed_loop_avg_dab.py
+++ new-closed-loop-dab/new_closed_loop_avg_dab.py
@@ -13,6 +13,7 @@ T_wave  = 1e-6
 T_math  = 1e-6
 
 V_in  = 780
+V_out = 780
 V1  = 780
 V2  = 780
 C   = 1e-6
@@ -32,4 +33,16 @@ phi_pu = 1-phi*fsw
 I = V1*D*(1-D)/(2*fsw*L)
 R_load = V1/I
 
-# print(I)
+P_c = 5e3
+R_load_c  = (V_out**2) / P_c 
+
+d_c = d*0.5
+
+Gdc     = ((R_load_c)/(2*V_out))*((V1*V2*(1-2*d_c)/(2*fsw*L)))
+tau     = (C*R_load_c)/2
+
+f_bw    = 0.05*fsw
+omega_c = 2*np.pi*f_bw
+
+K_p      = (tau*omega_c)/Gdc
+K_i      = K_p/tau
