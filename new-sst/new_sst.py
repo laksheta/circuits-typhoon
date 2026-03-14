dde51d7fd1cb8b166c560b261f9d79695e7a7da9
Laksheta <krishnalaksheta@gmail.com>
2026-03-14 23:56:46 +0700

    code(sst): add rectifier section

--- new-sst/new_sst.py
+++ new-sst/new_sst.py
@@ -103,9 +103,9 @@ Ki_i_inv    = omega_c_inv*R_inv
 #################
 
 
-T_plant_rect = T_math_dab#50e-6
-T_meas_rect   = T_math_dab#100e-6
-T_controller_rect = T_math_dab#100e-6
+T_plant_rect = 50e-6
+T_meas_rect   = 100e-6
+T_controller_rect = 100e-6
 
 V_grid_peak_rect = 220*np.sqrt(2)
 V_grid_rms_rect  = 220
