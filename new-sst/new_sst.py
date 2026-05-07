584f072db8fa1b072603392065b59fc674f8c501
Laksheta <krishnalaksheta@gmail.com>
2026-05-07 14:30:13 +0700

    code: update code

--- new-sst/new_sst.py
+++ new-sst/new_sst.py
@@ -103,9 +103,9 @@ Ki_i_inv    = omega_c_inv*R_inv
 #################
 
 
-T_plant_rect = 50e-6
-T_meas_rect   = 100e-6
-T_controller_rect = 100e-6
+T_plant_rect = 500e-6
+T_meas_rect   = 1000e-6
+T_controller_rect = 1000e-6
 
 V_grid_peak_rect = 220*np.sqrt(2)
 V_grid_rms_rect  = 220
